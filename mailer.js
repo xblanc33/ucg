df0b73c45973cf1bcfa11af3217454cd38fb495b
xavier blanc <xavier blanc>
2017-03-02 14:37:31 +0100

    mail

--- mailer.js
+++ mailer.js
@@ -2,10 +2,11 @@
 var nodemailer = require('nodemailer')
 
 module.exports.transporter = nodemailer.createTransport({
-    service: 'GandiMail',
-    auth: {
+	host: 'smtp1.dc0.gpaas.net',
+	port:'25'//,
+    /*auth: {
         user: 'webmaster@ucgradignan.fr',
         pass: 'webucg-33170@2017'
-    }
+    }*/
 })
 
