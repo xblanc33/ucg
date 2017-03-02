73033fe44cdd3b6b2704164e8c53bb618580bac4
xavier blanc <xavier blanc>
2017-03-02 18:31:14 +0100

    mail

--- mailer.js
+++ mailer.js
@@ -4,6 +4,7 @@ var nodemailer = require('nodemailer')
 module.exports.transporter = nodemailer.createTransport({
 	host: 'smtp1.dc0.gpaas.net',
 	port:'25',
+	secureConnection: false,
     auth: {
         user: 'webmaster@ucgradignan.fr',
         pass: 'webucg-33170@2017'
