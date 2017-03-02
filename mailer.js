9e478fbcecff179fa1e8cd98fc40f91890cd5137
xavier blanc <xavier blanc>
2017-03-02 18:41:35 +0100

    log mail error

--- mailer.js
+++ mailer.js
@@ -4,7 +4,8 @@ var nodemailer = require('nodemailer')
 module.exports.transporter = nodemailer.createTransport({
 	host: 'smtp1.dc0.gpaas.net',
 	port:'25',
-	secureConnection: false,
+	secure: false,
+	ignoreTLS:true,
     auth: {
         user: 'webmaster@ucgradignan.fr',
         pass: 'webucg-33170@2017'
